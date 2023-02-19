da9ab0df6ccb53fb50d9f9b936767517a2e25e8f
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-02-19 09:48:59 +0530

    search-option-width-updated

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -1,25 +1,49 @@
+import { useEffect,useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faSearch,faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import $ from 'jquery'
+
 
 const SearchBar = () => {
+    const [isSearchClicked,setIsSearchClicked] = useState<boolean>(false);
+    let searchBarWidth = $('.search-bar').outerWidth();
+    let searchDropDownWidth = $('.search-bar--dropdown').outerWidth();
+
+    useEffect(()=>{
+        if(isSearchClicked){
+            if(searchBarWidth && searchDropDownWidth){
+                let searchOptionWidth = searchBarWidth - searchDropDownWidth;
+                $('.search-bar--options').css('width',searchOptionWidth);
+            }
+        }
+    },[isSearchClicked,searchBarWidth,searchDropDownWidth])
+    
   return (
     <>
-    <div className="search-bar">
-        <button className='search-bar--dropdown'>
-         <label>All</label>
-         <FontAwesomeIcon icon={faCaretDown}/>
+      <div className="search-bar">
+        <button className="search-bar--dropdown">
+          <label>All</label>
+          <FontAwesomeIcon icon={faCaretDown} />
         </button>
-        <input type="text" className="search-bar--input" placeholder="Search for products, brands and more"/>
-        <button className='search-bar--button'>
-         <FontAwesomeIcon icon={faSearch}/>
+        <input
+          type="text"
+          className="search-bar--input"
+          placeholder="Search for products, brands and more"
+        />
+        <button
+          className="search-bar--button"
+          onClick={() => setIsSearchClicked(true)}
+        >
+          <FontAwesomeIcon icon={faSearch} />
         </button>
-    </div>
-    <div className="search-bar--options">
-        <label>Demo Data</label>
-    </div>
+      </div>
+      {isSearchClicked ? (
+        <div className="search-bar--options">
+          <label>Demo Data</label>
+        </div>
+      ) : null}
     </>
-  )
+  );
 }
 
 export default SearchBar
