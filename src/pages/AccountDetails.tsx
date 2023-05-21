4be805486c04319175a013d5392168898f47575a
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-05-21 15:37:40 +0530

    wip-auth

--- src/pages/AccountDetails.tsx
+++ src/pages/AccountDetails.tsx
@@ -7,14 +7,14 @@ const AccountDetails = () => {
   return (
     //TODO fix routing on button click for now routes are working when we enter from browser
     <>
-      <Link to="/account/login">
-        <Button variant="warning">
-          Login
-        </Button>
-      </Link>
       <Link to="/account/create-new-account">
         <Button variant="primary">
           Create New Account
+        </Button>
+      </Link>
+      <Link to="/account/login">
+        <Button variant="warning">
+          Login
         </Button>
       </Link>
       <Outlet />
